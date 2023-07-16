84396805040cc0d9550fa038e8f8e1dd2159d7ac
Dravelz-21 <simercsi@gmail.com>
2023-07-16 16:59:32 +0200

    Added Kick player with chat and a platform switch button

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let channelID = 'xqc';
 let live = false;
-   
+let platformSwitchBtn = document.getElementById('platformSwitch');
+
+
 async function checkStream(channel) {
     let response = await fetch(`https://kick.com/api/v2/channels/${channel}`);
     let data = await response.json();
@@ -9,4 +11,12 @@ async function checkStream(channel) {
             return setLive();
         } else { return setOffline()}
     }
-}
+};
+
+function setLive() {
+    live = true;
+};
+
+function setOffline() {
+    live = false;
+};
