e2ebb75b9ad69217ab7b2544d46968f0dcccde30
Dravelz-21 <simercsi@gmail.com>
2023-07-16 15:53:10 +0200

    New function to check stream status on Kick.

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+let channelID = 'xqc';
+let live = false;
+   
+async function checkStream(channel) {
+    let response = await fetch(`https://kick.com/api/v2/channels/${channel}`);
+    let data = await response.json();
+    if(response.status === 200){
+        if(data.livestream.is_live == true){
+            return setLive();
+        } else { return setOffline()}
+    }
+}
