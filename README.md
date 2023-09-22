077ca94d87bcd413e411a8456ab0d89ed9fb2d38
Dravelz <simercsi@gmail.com>
2023-09-22 03:05:40 +0200

    url prompt handling, portrait mode, null handling
    Initial Portrait CSS,
    If channelID == null then player defaults to monstercats twitch stream,
    URL Prompt handling for twitch,
    Added credits to README

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # twitch-viewer
 Webpage with embedded Twitch & Kick player and chat
 
+[DEMO](https://kick_twitch-viewer.netlify.app)
+
 ## Features
 
-- Channel change button above chat.
+- Channel change button top left.
 - Automatically switch stream if Kick stream either goes live or offline. (checks the same username that is entered in the prompt.)
 - 3rd party chat supports 7TV,BTTV,FFZ emotes.
-- Remember selected channel on page load
+- Remember selected channel on page load.
+- Enter /?channel={channel}&quality={720p60}&volume={0.5} to change channel, quality, volume on page load. Not all 3 prompt is required. Quality change only works with the Twitch player Kick didn't implement this feature yet in their player.
+- Early Portrait mode for mobile devices (CSS help welcome). 
+- Load [Monstercat](https://twitch.tv/monstercat) stream if no stream is selected.
 
 
 ## Why
 
-Kick's webpage is very heavy on Android TV's and theres no official app, this project aims to minimise cpu usage while making the chat experience better. 
+Kick's webpage is very heavy, this project aims to minimise cpu usage by embedding the player and making the chat experience better without any 3rd party extension. 
 
-Keep in mind this is a very barebones project and lots of spaghetti code but it gets the job done.
+Keep in mind this is a very barebones project but it gets the job done.
+
+## Install
+
+Clone the repo and use a webserver to host it. Keep in mind the twitch player needs HTTPS parent to work except if on 127.0.0.1, use a self signed cert for it.
+
+## Credits
+
+[Giambaj](https://www.giambaj.it/) for the [Twitch chat overlay](https://www.giambaj.it/twitch/jchat/).
+
+[Corard](https://github.com/Corard) for the [Kick chat overlay](https://corard.tv/projects/kick-chat).
--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-var channelID = '';
-let live = false;
-let alreadySwitched = false;
-var kickCheck = 1000;
-let platformSwitchBtn = document.getElementById('platformSwitch');
-let channelBtn = document.getElementById('channelSelector');
-
-let parentURL = "twitch-viewer-demo.netlify.app"; // for the demo page
+var channelID = ''; // channel ID for building the player and chat
+let volume = 0.5; // can be "0.1" to "1" for the twitch player
+let quality = "720p60";
+let live = false; // true if live on Kick.com
+let alreadySwitched = false; // false if on twitch player true if on Kick.com player
+var kickCheck = 2000; // how often the live check updates for Kick.com (ms)
+let platformSwitchBtn = document.getElementById('platformSwitch'); // was a button to change to Kick, now used for determining if player is Twitch or Kick (this is a bad implementation)
+let channelBtn = document.getElementById('channelSelector'); 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+document.body.onload = nullCheck;
+// Parent url needed for the twitch player embed
+let parentURL = "kick-twitch-viewer.netlify.app"; // for the demo page
 // let parentURL = "127.0.0.1"; // for local developement
 
-
+// fuction to check if the channel is live on Kick.com through the API every 2s
 async function checkStream(channel) {
     try {
         let response = await fetch(`https://kick.com/api/v2/channels/${channel}`);
@@ -16,37 +21,31 @@ async function checkStream(channel) {
         let data = await response.json();
         if(response.status === 200){
             if(data.livestream.is_live == true){
-                // console.log("[INFO] Live on KICK");
                 return setLive();
             }
         };
         } catch (err ) {
-            // console.log("[INFO] Not Live on KICK")
             return setOffline(); 
     }
 };
 
 function setLive() {
     live = true;
-    kickCheck = 10000;
+    // console.log("[INFO] Live on KICK");
 };
 
 function setOffline() {
     live = false;
-    kickCheck = 10000;
+    // console.log("[INFO] Not Live on KICK")
 };
 
+
+// current ui builder solution
 function switchPlatform() {
-    if(platformSwitchBtn.getAttribute('current') === 'twitch') {
-        document.getElementById('twitch-player').src = `https://player.kick.com/${channelID}?autoplay=true&muted=false&allowfullscreen=true`;
-        document.getElementById('twitch-chat').src = `https://kick-chat.corard.tv/v1/chat?user=${channelID}&font-size=Small&stroke=Off&animate=false&badges=true&commands=false&bots=false`;
-        platformSwitchBtn.setAttribute("current", "kick");
-        alreadySwitched = true;
-    } else if(platformSwitchBtn.getAttribute('current') === 'kick') {
-        document.getElementById('twitch-player').src = `https://player.twitch.tv/?channel=${channelID}&enableExtensions=false&muted=false&parent=${parentURL}&player=popout&quality=720p60&volume=0.2`;
-        document.getElementById('twitch-chat').src = `https://www.giambaj.it/twitch/jchat/v2/?channel=${channelID}&bots=true&hide_commands=true&size=1&font=2`;
-        platformSwitchBtn.setAttribute("current", "twitch");
-        alreadySwitched = false;
+    if(platformSwitchBtn.getAttribute('current') === 'twitch') { // if on Twitch switch to Kick
+        buildKick(channelID);
+    } else if(platformSwitchBtn.getAttribute('current') === 'kick') { // if on Kick switch to Twitch
+        buildTwitch(channelID, quality, volume)
     }
 }
 
@@ -77,19 +76,17 @@ function setCookie(cname, cvalue, exdays) {
 }
 
 function checkCookie() {
+
     let channel = getCookie("channel");
-    if (channel != "") {
-        channelID = channel
-        document.getElementById('twitch-player').src = `https://player.twitch.tv/?channel=${channel}&enableExtensions=false&muted=false&parent=${parentURL}&player=popout&quality=720p60&volume=0.2`;
-        document.getElementById('twitch-chat').src = `https://www.giambaj.it/twitch/jchat/v2/?channel=${channel}&bots=true&hide_commands=true&size=1&font=2`;
+    if(channel != null || channel != "null"){
+        buildTwitch(channel, quality);
+        nullCheck();
     } else {
-      channel = prompt("Set Channel", "xqc");
-      if (channel != "" && channel != null) {
-        channelID = channel
-        document.getElementById('twitch-player').src = `https://player.twitch.tv/?channel=${channel}&enableExtensions=false&muted=false&parent=${parentURL}&player=popout&quality=720p60&volume=0.2`;
-        document.getElementById('twitch-chat').src = `https://www.giambaj.it/twitch/jchat/v2/?channel=${channel}&bots=true&hide_commands=true&size=1&font=2`;
+        let selectedChannel = prompt("Set Channel", "xqc");
+        channelID = selectedChannel;
+        buildTwitch(selectedChannel, quality);
         setCookie("channel", channel, 365);
-      }
+        nullCheck();
     }
 }
 
@@ -108,12 +105,64 @@ function getCookie(cname) {
     return "";
 }
 
-
+function deleteCookie(name) {
+    document.cookie = `name; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+}
 function channelSelector() {
-    document.cookie = "channel=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    deleteCookie("channel=")
     let channel = prompt("Set Channel", "xqc");
     setCookie("channel", channel, 365);
     checkCookie();
 }
 
-checkCookie();
+function buildTwitch(channel, quality, volume) {
+    document.getElementById('twitch-player').src = `https://player.twitch.tv/?channel=${channel}&enableExtensions=false&parent=${parentURL}&player=popout&quality=${quality}&volume=${volume}&muted=false`;
+    document.getElementById('twitch-chat').src = `https://www.giambaj.it/twitch/jchat/v2/?channel=${channel}&bots=true&hide_commands=true&size=1&font=2`;
+    platformSwitchBtn.setAttribute("current", "twitch");
+    alreadySwitched = false;
+    channelID = channel;
+}
+
+function buildKick(channel) {
+    document.getElementById('twitch-player').src = `https://player.kick.com/${channel}?autoplay=true&muted=false&allowfullscreen=true`;
+    document.getElementById('twitch-chat').src = `https://kick-chat.corard.tv/v1/chat?user=${channel}&font-size=Small&stroke=Off&animate=false&badges=true&commands=false&bots=false`;
+    platformSwitchBtn.setAttribute("current", "kick");
+    alreadySwitched = true;
+    channelID = channel;
+}
+
+
+function checkQuery() {
+    let channelQuery = urlParams.get("channel");
+    let volume = urlParams.get("volume");
+    let quality = urlParams.get("quality");
+    channelID = channelQuery;
+    if(channelQuery == "" || channelQuery == null){
+        let c = getCookie("channel");
+        channelQuery = c;
+        channelID = c;
+    }
+    if(volume == "" || volume == null || volume == "null") {
+        volume = 1;
+    };
+    if(quality == "" || quality == null || volume == "null") {
+        quality = "720p60";
+    } else if(quality == "best") {
+        quality = "chunked";
+    }
+
+    buildTwitch(channelID, quality, volume);
+}
+
+if( queryString == "" || queryString == null || queryString == "null") {
+    checkCookie();
+} else if( queryString != "") {
+    checkQuery();
+}
+
+
+async function nullCheck(){
+    if(channelID == null || channelID == "null"){
+    buildTwitch("monstercat", "360p30", "0.5")
+    }
+}
